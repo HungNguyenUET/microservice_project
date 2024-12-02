9a84b879fa7695a6813049743116474da20a5f0d
hungnguyenuet <hungnguyenuet@gmail.com>
2024-12-02 20:01:07 +0700

    Init projects

---
+++ account-service/src/main/java/hungnv/account_service/controller/AccountController.java
@@ -0,0 +1,31 @@
+package hungnv.account_service.controller;
+
+import hungnv.account_service.dto.AccountDTO;
+import hungnv.account_service.entity.Account;
+import hungnv.account_service.service.IAccountService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/v1/accounts")
+@RequiredArgsConstructor
+public class AccountController {
+    private final IAccountService acService;
+    private final ModelMapper modelMapper;
+
+    public List<AccountDTO> getListAccounts() {
+
+        List<Account> accounts = acService.getListAccounts();
+
+        List<AccountDTO> listAccountDTO = modelMapper.map(
+                accounts,
+                new TypeToken<List<AccountDTO>>() {}.getType());
+
+        return listAccountDTO;
+    }
+}
---
+++ account-service/src/main/java/hungnv/account_service/service/IAccountService.java
@@ -0,0 +1,11 @@
+package hungnv.account_service.service;
+
+import hungnv.account_service.entity.Account;
+
+import java.util.List;
+
+public interface IAccountService {
+
+    List<Account> getListAccounts();
+
+}
