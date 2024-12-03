741dfc040365f1056802cf32f02217fbb2ebdf0a
Namlt1 <thanhnam0810.tnl@gmail.com>
2024-12-03 21:42:09 +0700

    add eureka server and config client

--- account-service/src/main/java/hungnv/account_service/AccountServiceApplication.java
+++ account-service/src/main/java/hungnv/account_service/AccountServiceApplication.java
@@ -2,10 +2,10 @@ package hungnv.account_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
