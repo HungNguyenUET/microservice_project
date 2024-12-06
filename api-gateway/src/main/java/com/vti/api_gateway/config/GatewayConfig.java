05f43a75f404c5be32c07988e5069acd8321d73c
PhucQuangg <nguyenhoangphuc020803@gmail.com>
2024-12-06 22:31:34 +0700

    update cÃonfig and gateway

---
+++ api-gateway/src/main/java/com/vti/api_gateway/config/GatewayConfig.java
@@ -0,0 +1,24 @@
+package com.vti.api_gateway.config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    @Autowired
+    AuthenticationFilter filter;
+
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("account-service", r -> r.path("/api/v1/accounts/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://account-service"))
+                .route("auth-service", r -> r.path("/api/v1/auth/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("lb://auth-service"))
+                .build();
+
+    }
+}
