59030e6b6b15523ce35a4ff45970235499db538b
hungnguyenuet <hungnguyenuet@gmail.com>
2025-01-13 21:47:23 +0700

    update lesson_10

--- account-service/src/main/java/hungnv/account_service/AccountServiceApplication.java
+++ account-service/src/main/java/hungnv/account_service/AccountServiceApplication.java
@@ -8,9 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@SpringBootApplication
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
